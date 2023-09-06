e8e5e4e1fa7438498f9cf87bb43d5ff3fdbc8c69
emirbarancelik <141326397+emirbarancelik@users.noreply.github.com>
2023-09-06 20:18:55 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+//syntax o dilin yazım kurallarıdır
+// \n 
+printf("merhaba sakarya \n");
+printf("\nmerhaba bursa");
+
+
+	return 0;
+} 
